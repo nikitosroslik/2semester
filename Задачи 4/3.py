0792f10e78a51693525012456ebd77293222b3d1
nikitosroslik <147470643+nikitosroslik@users.noreply.github.com>
2024-05-19 20:38:44 +0500

    Update 3.py

--- Задачи 4/3.py
+++ Задачи 4/3.py
@@ -2,7 +2,7 @@ def fad(a, b):
     if (b == 1):
         return (a)
     if (b != 1):
-        return (a * Vika(a, b - 1))
+        return (a * fad(a, b - 1))
 a = int(input("Введите число: "))
 b = int(input("Введите его степень: "))
 print("Результат возведения в степень равен:", fad(a, b))
